846530c689ba872287556987ec04811b65715466
KangChangSeon <121225359+KangChangSeon@users.noreply.github.com>
2023-02-16 17:02:10 +0900

    Add files via upload

--- test1.cpp
+++ test1.cpp
@@ -1,10 +1,9 @@
 #include <stdio.h>
 void main()
 {
-	printf("한글이름 : %s\n", "김주한");
-	printf("영문이름 : %s\n", "Kim Juhan");
-	printf("반 : %c\n", 'A');
-	printf("나이 : %d\n", 23);
-	printf("키 : %.1f cm\n", 176.3);
-	printf("휴대전화 : %s\n", "010-0043-1004");
+	char ch1 = 'a', ch2 = 66;
+	printf("ch1의 값 %c\n", ch1);
+	printf("ch1의 정수의 값 %d\n", ch1);
+	printf("ch2의 값 %c\n", ch2);
+	printf("ch1의 값 %d\n", ch2);
 }
